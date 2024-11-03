0e4d02ed5f6bbf23c3bce6dcbe6e5056cc607fb3
Pavel <pamitrovel@gmail.com>
2024-11-03 18:57:10 +0300

    add OUTPUT_ORDER_COUNTER, ADDED_TO_CART_MARGARITA

--- src/main/java/PizzaMenuMessage.java
+++ src/main/java/PizzaMenuMessage.java
@@ -1,3 +1,5 @@
 public class PizzaMenuMessage {
     public static final String OUTPUT_PIZZA_ORDER_POPUP = "Простой заказ";
+    public static final String OUTPUT_ORDER_COUNTER = "1";
+    public static final String ADDED_TO_CART_MARGARITA = "Пицца \"Маргарита\" (21см)";
 }
