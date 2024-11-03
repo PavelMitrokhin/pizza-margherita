9ea099a3b525fa05c559bf918f793325f2a5e543
Pavel <pamitrovel@gmail.com>
2024-11-03 16:15:44 +0300

    add PizzaMenuXpath

---
+++ src/main/java/PizzaMenuXpath.java
@@ -0,0 +1,6 @@
+public class PizzaMenuXpath {
+    public static final String HYPERLINK_PIZZA_MENU_XPATH = "/html/body/div[1]/div[3]/ul/li[1]/a/img";
+    public static final String BUTTON_ORDER_PIZZA_MARGARITA_XPATH = "//*[@id=\"1\"]/div/div[2]/div[3]/div[3]";
+
+    public static final String OUTPUT_ORDER_PIZZA_POPUP_XPATH = "//*[@id=\"firstTab\"]/span";
+}
