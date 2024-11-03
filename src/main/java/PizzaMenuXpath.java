83518746a082b90fa8c3cfb8c34aa8de03578c38
Pavel <pamitrovel@gmail.com>
2024-11-03 19:06:24 +0300

    refactor HYPERLINK_DRINKS_XPATH, BUTTON_ADD_TO_CART_COLA_XPATH

--- src/main/java/PizzaMenuXpath.java
+++ src/main/java/PizzaMenuXpath.java
@@ -3,6 +3,8 @@ public class PizzaMenuXpath {
     public static final String BUTTON_ADD_TO_CART_PIZZA_MARGARITA_XPATH = "//*[@id=\"1\"]/div/div[2]/div[3]/div[3]";
     public static final String BUTTON_CONFIRM_ADD_TO_CART_PIZZA_MARGARITA_XPATH = "//*[@id=\"order_pizza_popup\"]/div[2]/div[2]/div[2]/div[1]/div[3]/div[4]/button";
     public static final String HYPERLINK_CART_XPATH = "//*[@id=\"basket_placeholder\"]/div/div/div[1]/div[1]/a";
+    public static final String HYPERLINK_DRINKS_XPATH = "/html/body/div[1]/div[3]/ul/li[6]/a";
+    public static final String BUTTON_ADD_TO_CART_COLA_XPATH = "/html/body/div[1]/div[5]/div[2]/div[2]/div[2]/div[1]/div/div[2]/div[2]/button/span/span";
 
     public static final String OUTPUT_ORDER_PIZZA_POPUP_XPATH = "//*[@id=\"firstTab\"]/span";
     public static final String OUTPUT_ORDER_COUNTER_XPATH = "//*[@id=\"basket_placeholder\"]/div/div/div[1]/div[1]/a/div/div";
