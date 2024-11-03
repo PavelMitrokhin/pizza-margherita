e112ef4d5b34634c65b55f4b3c81d85146c3ce90
Pavel <pamitrovel@gmail.com>
2024-11-03 19:21:36 +0300

    add OUTPUT_ADDED_COLA_XPATH

--- src/main/java/PizzaMenuXpath.java
+++ src/main/java/PizzaMenuXpath.java
@@ -9,4 +9,5 @@ public class PizzaMenuXpath {
     public static final String OUTPUT_ORDER_PIZZA_POPUP_XPATH = "//*[@id=\"firstTab\"]/span";
     public static final String OUTPUT_ORDER_COUNTER_XPATH = "//*[@id=\"basket_placeholder\"]/div/div/div[1]/div[1]/a/div/div";
     public static final String OUTPUT_ADDED_MARGARITA_XPATH = "//*[@id=\"basket_expand\"]/table/tbody/tr[1]/td[2]/div[1]";
+    public static final String OUTPUT_ADDED_COLA_XPATH = "//*[@id=\"basket_expand\"]/table/tbody/tr[2]/td[2]/div[1]";
 }
