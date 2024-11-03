81093ae9a980663e83960f25da300b36c1afbbfb
Pavel <pamitrovel@gmail.com>
2024-11-03 19:12:57 +0300

    add ADDED_TO_CART_COLA

--- src/main/java/PizzaMenuMessage.java
+++ src/main/java/PizzaMenuMessage.java
@@ -2,4 +2,5 @@ public class PizzaMenuMessage {
     public static final String OUTPUT_PIZZA_ORDER_POPUP = "Простой заказ";
     public static final String OUTPUT_ORDER_COUNTER = "1";
     public static final String ADDED_TO_CART_MARGARITA = "Пицца \"Маргарита\" (21см)";
+    public static final String ADDED_TO_CART_COLA = "Кока-Кола 1л";
 }
